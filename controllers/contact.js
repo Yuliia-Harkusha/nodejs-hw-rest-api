2498204bf92925a22092a528cf58207a17c86dd7
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-12-27 00:06:37 +0100

    add filter favorite

--- controllers/contact.js
+++ controllers/contact.js
@@ -8,12 +8,13 @@ const { HttpError } = require("../helpers");
 const listContacts = async (req, res, next) => {
   try {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 5 } = req.query;
+    const { page = 1, limit = 5, favorite } = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({ owner }, "", { skip, limit }).populate(
-      "owner",
-      "email"
-    );
+    const result = await Contact.find(
+      favorite ? { owner, favorite } : { owner },
+      "",
+      { skip, limit }
+    ).populate("owner", "email subscription");
     res.json(result);
   } catch (error) {
     next(error);
@@ -52,7 +53,8 @@ const addContact = async (req, res, next) => {
 const removeContact = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const result = await Contact.findByIdAndRemove(id);
+    const { _id: owner } = req.user;
+    const result = await Contact.findOneAndDelete({ id, owner });
 
     if (!result) {
       throw HttpError(404, "Not found");
@@ -72,8 +74,11 @@ const updateContact = async (req, res, next) => {
     }
 
     const { id } = req.params;
+    const { _id: owner } = req.user;
 
-    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    const result = await Contact.findOneAndUpdate({ id, owner }, req.body, {
+      new: true,
+    });
     if (!result) {
       throw HttpError(404, "Not found");
     }
